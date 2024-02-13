6727c1d24aa72ad27f517ee83c5e4536a48cce88
stefanbalazclear <stefanbalaz@hotmail.com>
2024-02-13 09:50:53 +0100

    auth try

--- controllers/partner.js
+++ controllers/partner.js
@@ -11,6 +11,52 @@ const createPartner = async (req, res) => {
   }
 };
 
+const registerPartner = async (req, res) => {
+  try {
+    const { userName, email, password } = req.body.partnerRegistration;
+    const newPartner = new Partner({
+      partnerRegistration: {
+        userName,
+        email,
+        password,
+      },
+      partnerRole: "customer",
+      partnerRelationType: "private",
+    });
+
+    const partner = await newPartner.save();
+
+    res.status(201).json({ success: true, data: partner });
+  } catch (error) {
+    res.status(500).json({ success: false, error: error.message });
+  }
+};
+
+const loginPartner = async (req, res) => {
+  try {
+    const { userName, password } = req.body;
+
+    // Find the user by userName or email and check the password
+    const user = await Partner.findOne({
+      $or: [
+        { "partnerRegistration.userName": userName },
+        { "partnerRegistration.email": userName },
+      ],
+      "partnerRegistration.password": password,
+    });
+
+    if (user) {
+      // User found, handle successful login
+      res.status(200).json({ success: true, data: "Login successful" });
+    } else {
+      // User not found or password incorrect, handle login failure
+      res.status(401).json({ success: false, error: "Invalid credentials" });
+    }
+  } catch (error) {
+    res.status(500).json({ success: false, error: error.message });
+  }
+};
+
 const getAllPartners = async (req, res) => {
   try {
     const partners = await Partner.find();
@@ -37,6 +83,8 @@ const getPartnerById = async (req, res) => {
 
 module.exports = {
   createPartner,
+  registerPartner,
+  loginPartner,
   getAllPartners,
   getPartnerById,
 };
